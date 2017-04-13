e96e0e895729232d52a722ff3a8023968dc99222
Aleksandr Necheukhin <necheyhin_a_v@mail.ru>
2017-04-13 18:07:05 +0700

    Добавлен рабочий шаблон

---
+++ CSActiveX/CSActiveXCtrl.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CSActiveX
+{
+    partial class CSActiveXCtrl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.lbFloatProperty = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CSActiveXCtrl";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FloatProperty:";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(15, 75);
+            this.tbMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(203, 22);
+            this.tbMessage.TabIndex = 3;
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(227, 73);
+            this.btnMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(121, 28);
+            this.btnMessage.TabIndex = 4;
+            this.btnMessage.Text = "Сообщение";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // lbFloatProperty
+            // 
+            this.lbFloatProperty.AutoSize = true;
+            this.lbFloatProperty.Location = new System.Drawing.Point(115, 43);
+            this.lbFloatProperty.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbFloatProperty.Name = "lbFloatProperty";
+            this.lbFloatProperty.Size = new System.Drawing.Size(16, 17);
+            this.lbFloatProperty.TabIndex = 2;
+            this.lbFloatProperty.Text = "0";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(14, 105);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(227, 109);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(227, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // CSActiveXCtrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.lbFloatProperty);
+            this.Controls.Add(this.btnMessage);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "CSActiveXCtrl";
+            this.Size = new System.Drawing.Size(571, 329);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btnMessage;
+        private System.Windows.Forms.Label lbFloatProperty;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ CSActiveX/CSActiveXCtrl.cs
@@ -0,0 +1,139 @@
+﻿/*
+ Пример ActiveX контролла
+ */
+#region Using directives
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Reflection;
+using System.Security.Permissions;
+#endregion
+
+
+namespace CSActiveX
+{
+
+    /// <summary>
+    /// Интерфейс задает общие свойства и функции которые необходимо
+    /// реализовать в контроле контрол наследует данный интерфейс
+    /// </summary>
+    public interface AxCSActiveXCtrl
+    {
+        //Свойства
+        float FloatProperty1 { get; set; }
+        //Методы
+        string HelloWorld1();
+    }
+
+    /// <summary>
+    /// Открытый интерфейс, доступный для внешней среды описывающий события контрола
+    /// Данный интерфейс связывается с классом посредством ComSourceInterfaces()
+    /// Без данного интерфейса не будет возможности отлавливать события во внешней среде
+    /// </summary>
+    [Guid("901EE2A0-C47C-43ec-B433-985C020051D5")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface AxCSActiveXCtrlEvents
+    {
+        // Необходимо явно определять DISPID для каждого события, иначе,
+        // адрес обратного вызова не может быть найден при вызове события
+        [DispId(1)]
+        void Click1();
+        [DispId(2)]
+        void FloatPropertyChanging1(float NewValue);
+    }
+
+    /// <summary>
+    /// Собственно класс Контролла наследующий UserControl и интерфейс
+    /// Интерфейс для предоставления событий во внешнюю среду
+    /// подключается с помощью атрибута ComSourceInterfaces()
+    /// </summary>
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(AxCSActiveXCtrlEvents))]
+    [Guid("D4675E22-985D-48D0-86B4-D86581FBDF81")]
+    public partial class CSActiveXCtrl : UserControl, AxCSActiveXCtrl
+    {
+        //В данной секции происходит инициализация контрола
+        //Создание всех видимых объектов и т.д.
+        #region Initialization
+        public CSActiveXCtrl()
+        {
+            InitializeComponent();
+
+            // Подключение к обработчику событий клик по контролу
+            base.Click += new EventHandler(CSActiveXCtrl_Click);
+
+            // Установка автоскрола, если часть контрола не видна
+            this.AutoScroll = true; 
+        }
+
+        #endregion
+
+        //В данной секции отображены все свойства контрола
+        //Все открытые свойства доступны внешней среде
+        #region Properties
+        //Переменная, хранящая значение для свойства FloatProperty
+        private float fField = 0;
+
+        //Открытое свойство, доступное внешней среде с get и set
+        public float FloatProperty1
+        {
+            get { return this.fField; }
+            set 
+            {
+                // При установке свойства вызывается событие FloatProperyChanging(newValue)
+                if (null != FloatPropertyChanging1)
+                    FloatPropertyChanging1(value);
+                this.fField = value;
+                this.lbFloatProperty.Text = value.ToString();   //Обновление текста о текущем размере float
+            }
+        }
+        #endregion
+
+        // В данной секции показан метод, доступный внешней среде, т.к. является public
+        #region Methods
+        //Функция возвращает строку либо во внешнюю среду либо может выполняться средой .NET
+        public string HelloWorld1()
+        {
+            return "Сообщение Hello World";
+        }
+        #endregion
+
+        // В данной секции показаны примеры различных событий
+        // Для события, которое может быть отправлено наружу(т.е. вне ActiveX) необходимо:
+        // 1) Объявить делегат с необходимым заголовком функции, т.е. тип возвращаемого значения, имя и параметры
+        // 2) Создать новое событие типа delegate и необходимым именем
+        // 3) Произвести новое событие.
+        // 4) Также данное событие должно быть описано в интерфейсе с обязательным DispID
+        #region Events
+        //Событие отсылаемое во внешнюю среду без параметра
+        [ComVisible(false)]
+        public delegate void ClickEventHandler();
+        public new event ClickEventHandler Click1 = null;
+        void CSActiveXCtrl_Click(object sender, EventArgs e)
+        {
+            if (null != Click1) Click1(); // Произвести новое событие Click.
+        }
+
+        //Событие отсылаемое во внешнюю среду с параметром
+        [ComVisible(false)]
+        public delegate void FloatPropertyChangingEventHandler(float NewValue);
+        public event FloatPropertyChangingEventHandler FloatPropertyChanging1 = null;
+
+        //Событие обрабатываемое полностью внутри приложения .NET и не может контроллироваться внешней средой
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            if (tbMessage.Text == "")
+                MessageBox.Show("Заполните что-нибудь в текстбокс");
+            else
+                MessageBox.Show(tbMessage.Text, "Сообщение из текстбокса",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion
+    }
+}
